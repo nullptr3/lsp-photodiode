e6d2edb047fc5119aa4ec5142588659fece77705
nullptr3 <bencoopernotes@gmail.com>
2024-01-22 14:00:59 -0800

    cal. update

--- calibration/calibration_curve.py
+++ calibration/calibration_curve.py
@@ -1,119 +1,129 @@
-# include date and time support libraries
-from datetime import datetime
-from datetime import timedelta
-
-# include graphing libraries
-import matplotlib.pyplot as plt
-
-# best fit function libraries
-import numpy as np
+# curve fitting support library
 from scipy.optimize import curve_fit
 
-#PST vs PDT (8-hour UTC shift, 7-hour UTC shift)
+# graphing library
+import matplotlib.pyplot as plt
 
-def parse_sort(parsedData):
-    n = len(parsedData)
+# data science tools
+import pandas as pd
+import numpy as np
 
-    s = False
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if parsedData[j][0] > parsedData[j + 1][0]:
-                s = True
-                parsedData[j], parsedData[j + 1] = parsedData[j + 1], parsedData[j]
+# lin = a * x + b
+# exp = c * np.exp((x-f)/100*h) + g
+# return np.where(x < f, lin, exp)
 
-    if not s:
-        return
+def licor(filename : str) -> pd.DataFrame:
+    """Parses data from Licor file and
+    generates a data frame containing only
+    valid datetime and par information.
 
+    Args:
+        filename (str): file name w/ extension
 
-def get_par(filename):
-    par_data = []
-
-    file = open(filename, "r")
-    for line in file:
-        data = line.split()
-        if data[0] == "1":
-            try:
-                date = datetime.strptime(
-                    data[1] + " " + data[2][0:-3], "%Y-%m-%d %H:%M"
-                )
-                par_data.append([date, float(data[3])])
-            except ValueError:
-                continue
-
-    file.close()
-    return par_data
-
-
-def get_ada(filename):
-    ada_data = []
-
-    file = open(filename, "r")
-    for line in file:
-        data = line.split(",")
-        try:
-            date = datetime.strptime(data[3][0:-7], "%Y-%m-%d %H:%M")
-            date = date - timedelta(hours=7)
-            ada_data.append([date, float(data[1])])
-        except ValueError:
-            continue
-
-    file.close()
-    return ada_data
-
-
-def setup(par_filename, ada_filename, truncate):
-    data = [[], []]
-
-    par = get_par(par_filename)
-    ada = get_ada(ada_filename)
-
-    parse_sort(par)
-    parse_sort(ada)
-
-    par = par[truncate:-1]
-    ada = ada[truncate:-1]
-
-    for par_entry in par:
-        for ada_entry in ada:
-            if par_entry[0] == ada_entry[0]:
-                data[0].append(ada_entry[1])
-                data[1].append(par_entry[1])
-            elif par_entry[0] < ada_entry[0]:
-                break
-
-    return data
-
-
-def bestfit(data):
-    popt, pcov = curve_fit(
-        lambda t, a, b, c: t / a + np.exp((t - c) / b),
-        data[0],
-        data[1],
-        p0=(13.6, 61, 1400),
-    )
-
-    return popt[0], popt[1], popt[2]
-
-
-if __name__ == "__main__":
-    data = setup("June2QuantumData.txt", "Photodiode_(mV)-20230602-2322.csv", 500)
-    a, b, c = bestfit(data)
-
-    plt.scatter(data[0], data[1], s=1)
-
-    x = np.linspace(min(data[0]), max(data[0]), 1000)
-
-    plt.plot(x, x / a + np.exp((x - c) / b), "-k")
-
-    plt.title("Photodiode Calibration Curve")
-    plt.xlabel("Photodiode (mV)")
-    plt.ylabel("PAR")
+    Returns:
+        pd.DataFrame: valid Licor dataframe
+    """
     
-    corr_matrix = np.corrcoef(data[1], data[1] / a + np.exp((data[1] - c) / b))
-    corr = corr_matrix[0, 1]
-    R_sq = corr**2
+    # create dataframe from txt file
+    df = pd.read_csv(filepath_or_buffer=filename,
+                     usecols=(range(3)),
+                     header=None,
+                     sep='\t')
 
-    print(f"f(x) = x / {a} + e^((x - {c}) / {b})")
-    print("R_sq:", R_sq)
+    # rename columns for clarity
+    df.columns = ['valid','datetime','par']
+
+    # convert PAR column into numeric datatype
+    df['par'] = pd.to_numeric(errors='coerce', arg=df['par'])
     
-    plt.savefig("plot.png")
+    # remove all invalid and irrelevant data
+    df.drop(labels=df[df.iloc[:, 0] != 1].index, inplace=True)
+    df.dropna(subset=['par'], inplace=True)
+    df.drop(labels='valid', axis=1, inplace=True)
+
+    # convert datetime column into datetime datatype
+    df['datetime'] = pd.to_datetime(arg=df['datetime'].str[:-3])
+    df['datetime'] = df['datetime'].dt.tz_localize('America/Los_Angeles')
+
+    # recalculate indices
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+def adafruit(filename : str) -> pd.DataFrame:
+    """Parses data from AdaFruitIO file and
+    generates a data frame containing only
+    valid datetime and voltage information
+
+    Args:
+        filename (str): file name w/ extension
+
+    Returns:
+        pd.DataFrame: valid AdaFruit dataframe
+    """
+
+    # create dataframe from csv file
+    df = pd.read_csv(filename, usecols=([1,3]))
+
+    # rename columns for clarity
+    df.columns = ['mv', 'datetime']
+    df = df[['datetime', 'mv']]
+    
+    # convert mv into numerical data
+    df['mv'] = pd.to_numeric(df['mv'])
+
+    # convert datetime column into datetime datatype
+    df['datetime'] = pd.to_datetime(arg=df['datetime'].str[:-7])
+    df['datetime'] = df['datetime'].dt.tz_localize('UTC')
+
+    # convert to correct time zone
+    df['datetime'] = df['datetime'].dt.tz_convert('America/Los_Angeles')
+
+    return df
+
+def func(x, a, b, c, d):
+    log = a * np.log(x + b) + c
+    return log
+
+def match(licor_filename : str, ada_filename) -> pd.DataFrame:
+    # parse data from given source files
+    licor_df = licor(licor_filename)
+    ada_df = adafruit(ada_filename)
+
+    # perform data point matching
+    df = pd.merge_asof(ada_df, licor_df, on='datetime')
+
+    # customizable noise threshold
+    par_threshold = 1
+    mv_threshold = 1
+
+    # remove dead data points
+    df = df[df['par'].diff().abs() > par_threshold]
+    df = df[df['mv'].diff().abs() > mv_threshold]
+    df = df.reset_index(drop=True)
+    
+    # sort data frame in-place based on mv
+    df.sort_values(by='mv', inplace=True)
+    
+    # group data based on threshold proximity
+    threshold = 5
+    
+    bins = np.arange(df['mv'].min(), 
+                     df['mv'].max() + threshold, 
+                     threshold)
+    
+    df['bin'] = pd.cut(df['mv'], bins=bins, labels=False)
+    
+    df = df.groupby('bin').mean().reset_index()
+    
+    plt.scatter(df['mv'], df['par'], s=1)
+    
+    # curve fitting
+    params, covariance = curve_fit(func, df['mv'], df['par'], method='lm', ftol=1e-32, maxfev=1000000)
+    a, b, c, d = params
+    
+    plt.plot(df['mv'], func(df['mv'], a, b, c, d), 'k')
+    plt.title(f'Photodiode-to-Par: {a}*log({d} * x + {b}) + {c}')
+    plt.show()
+
+match("test_0830_2.txt", "pv.csv")
